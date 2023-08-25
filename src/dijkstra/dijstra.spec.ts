968159cbe39b5ddc4b579879ef900fcaff457a10
Jason Nordheim <jason.nordheim@gmail.com>
2023-08-25 00:20:03 -0600

    added tests

---
+++ src/dijkstra/dijstra.spec.ts
@@ -0,0 +1,27 @@
+import { GraphNode } from "./GraphNode";
+import { dijkstra } from "./dijkstra";
+
+describe("Dijkstra's Algorithm", () => {
+  const nodeA = new GraphNode("A");
+  const nodeB = new GraphNode("B");
+  const nodeC = new GraphNode("C");
+  const nodeD = new GraphNode("D");
+
+  nodeA.addNeighbor(nodeB, 5);
+  nodeA.addNeighbor(nodeC, 2);
+  nodeB.addNeighbor(nodeD, 3);
+  nodeC.addNeighbor(nodeD, 1);
+
+  it("should find the shortest path between two nodes", () => {
+    const path = dijkstra(nodeA, nodeD);
+    const pathIds = path!.map((node) => node.id);
+
+    expect(pathIds).toEqual(["A", "C", "D"]);
+  });
+
+  it("should handle when no path exists", () => {
+    const path = dijkstra(nodeB, nodeC);
+
+    expect(path).toBeUndefined();
+  });
+});
