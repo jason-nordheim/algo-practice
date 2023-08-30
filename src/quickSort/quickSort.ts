6ca33953b5de5084ac4409613511b6a69c16246b
Jason Nordheim <jason.nordheim@gmail.com>
2023-08-30 15:43:55 -0600

    added quick sort function

---
+++ src/quickSort/quickSort.ts
@@ -0,0 +1,25 @@
+/**
+ * @description Sorting function that uses a pivot element
+ * to create sub arrays that are sorted recursively
+ * - Divide and Conquer algorithm
+ * @param arr array to be sorted
+ */
+export const quickSort = (arr: number[]): number[] => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const pivotValue = arr[Math.floor(arr.length / 2)],
+    left: number[] = [],
+    right: number[] = [];
+
+  for (const num of arr) {
+    if (num < pivotValue) {
+      left.push(num);
+    } else if (num > pivotValue) {
+      right.push(num);
+    }
+  }
+
+  return [...quickSort(left), pivotValue, ...quickSort(right)];
+};
