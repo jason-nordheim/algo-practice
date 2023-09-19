24de512d7f72660d21e9f22b35c5c7049cd43ea3
Jason Nordheim <jason.nordheim@gmail.com>
2023-09-19 13:26:47 -0600

    fixed algorithm and updated comments

--- src/leetCode/maxSubsequenceScore.test.ts
+++ src/leetCode/maxSubsequenceScore.test.ts
@@ -7,9 +7,9 @@ describe("maxSubsequenceScore", () => {
     expect(maxScore(nums1, nums2, 3)).toEqual(12);
   });
 
-   it("input of nums1 = [1,3,4,2], nums2 = [2,1,4,4], k = 3 returns 12", () => {
-    const nums1 = [1, 3, 3, 2];
-    const nums2 = [2, 1, 3, 4];
-    expect(maxScore(nums1, nums2, 3)).toEqual(12);
+  it("input of nums1 = [4,2,3,1,1], nums2 = [2,1,4,4], k = 1 returns 30", () => {
+    const nums1 = [4, 2, 3, 1, 1];
+    const nums2 = [7, 5, 10, 9, 6];
+    expect(maxScore(nums1, nums2, 1)).toEqual(30);
   });
 });
--- src/leetCode/maxSubsequenceScore.ts
+++ src/leetCode/maxSubsequenceScore.ts
@@ -16,53 +16,47 @@
  *
  * Solution:
  * 1) Implemented using binary search
- * 2) Time Complexity => O(log(k) * n)
+ * 2) Time Complexity => O(n log n) - quasi-linear
  *    - k = desired length of subsequence
  *    - n = the length of the input arrays
- * 3) Space Complexity => O(1) (e.g. "constant")
+ * 3) Space Complexity =>O(n) - linear
  */
-export const maxScore = (nums1: number[], nums2: number[], k: number): number => {
-  // return maxScore;
+export function maxScore(nums1: number[], nums2: number[], k: number): number {
   const n = nums1.length;
-  let left = 0;
-  let right = k;
-  let result = 0;
+  const pairs = new Array(n);
+  for (let i = 0; i < n; i++) {
+    pairs[i] = [nums1[i], nums2[i]];
+  }
+  pairs.sort((a, b) => b[1] - a[1]);
 
-  while (left <= right) {
-    const i = left + Math.floor((right - left) / 2);
-    const j = k - i; // Calculate the corresponding number of elements to take from nums2
+  const heap = new Array();
+  let sum = 0;
+  for (let i = 0; i < k; i++) {
+    sum += pairs[i][0];
+    binaryInsert(heap, pairs[i][0]);
+  }
 
-    // Find the minimum value from nums2 for the current subsequence
-    let min2 = Infinity;
-    for (let x = 0; x < n; x++) {
-      if (x < i) {
-        min2 = Math.min(min2, nums2[x]);
-      } else if (x >= n - j) {
-        min2 = Math.min(min2, nums2[x]);
-      }
-    }
-
-    let score = 0;
-    let sum1 = 0;
-
-    // Calculate the score for the current subsequence
-    for (let x = 0; x < n; x++) {
-      if (x < i) {
-        sum1 += nums1[x];
-      } else if (x >= n - j) {
-        sum1 += nums1[x];
-      }
-    }
-
-    score = sum1 * min2;
-    result = Math.max(result, score);
-
-    if (i < k && nums1[i] < nums2[n - j - 1]) {
-      left = i + 1;
-    } else {
-      right = i - 1;
-    }
+  let result = sum * pairs[k - 1][1];
+  for (let i = k; i < n; i++) {
+    sum += pairs[i][0] - heap.shift();
+    binaryInsert(heap, pairs[i][0]);
+    result = Math.max(result, sum * pairs[i][1]);
   }
 
   return result;
-};
+}
+
+function binaryInsert(arr: number[], item: number) {
+  let left = 0;
+  let right = arr.length;
+
+  while (left < right) {
+    let mid = (left + right) >> 1;
+    if (arr[mid] > item) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+  arr.splice(left, 0, item);
+}
