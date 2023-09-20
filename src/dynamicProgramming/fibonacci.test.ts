3bd2c7ea4a2344caba7710f31296df71010da1e6
Jason Nordheim <jason.nordheim@gmail.com>
2023-09-20 00:12:58 -0600

    added fibonacci

---
+++ src/dynamicProgramming/fibonacci.test.ts
@@ -0,0 +1,54 @@
+import { fibonacci, memoizedFibonacci } from "./fibonacci";
+
+const SMALL_FIBS: [number, number][] = [
+  // nth, expectedResult
+  [1, 1],
+  [2, 1],
+  [3, 2],
+  [4, 3],
+  [7, 13],
+];
+
+describe("functionality", () => {
+  it.each(SMALL_FIBS)("fibonacci(%d)  returns %d", (nthNumber, expectedResult) => {
+    expect(fibonacci(nthNumber)).toEqual(expectedResult);
+  });
+  it.each(SMALL_FIBS)("fibinacci(%d) returns %d", (nthNumber, expectedResult) => {
+    expect(memoizedFibonacci(nthNumber)).toEqual(expectedResult);
+  });
+});
+
+//[50, 12586269025]
+const LARGE_FIBS: [number, number][] = [
+  [10, 55],
+  [30, 832040],
+];
+
+describe("performance", () => {
+  it.each(LARGE_FIBS)(
+    "When running both functions, memoizedFibonacci finishes in less time",
+    async (nthNumber, expectedResult) => {
+      const calculate = async () => {
+        const start = performance.now();
+        const result = fibonacci(nthNumber);
+        const end = performance.now();
+        return { start, end, result };
+      };
+
+      const memoizedCalculate = async () => {
+        const start = performance.now();
+        const result = memoizedFibonacci(nthNumber);
+        const end = performance.now();
+        return { start, end, result };
+      };
+
+      const [calculateResults, memoizedCalculateResults] = await Promise.all([calculate(), memoizedCalculate()]);
+
+      expect(calculateResults.result).toEqual(expectedResult);
+      expect(memoizedCalculateResults.result).toEqual(expectedResult);
+      const calculateRunTime = calculateResults.end - calculateResults.start;
+      const memoizedRunTime = memoizedCalculateResults.end - memoizedCalculateResults.start;
+      expect(calculateRunTime).toBeGreaterThan(memoizedRunTime);
+    }
+  );
+});
---
+++ src/dynamicProgramming/fibonacci.ts
@@ -0,0 +1,30 @@
+/**
+ * Recursive algorithm for calculating the nth fibonacci number
+ * @param n request fibonacci number
+ * @returns the nth fibonacci number
+ */
+export const fibonacci = (n: number): number => {
+  if (n <= 2) {
+    return 1;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+/**
+ * Memoized version of the algorithm for finding the nth fibonacci number
+ * @param n request fibonacci number
+ * @param seen map of previously seen fibonacci numbers
+ * @returns the nth fibonacci number
+ */
+export const memoizedFibonacci = (n: number, seen: Map<number, number> = new Map()): number => {
+  if (n <= 2) {
+    return 1;
+  }
+
+  if (seen.has(n)) {
+    return seen.get(n)!;
+  }
+
+  seen.set(n, fibonacci(n - 1) + fibonacci(n - 2));
+  return seen.get(n)!;
+};
