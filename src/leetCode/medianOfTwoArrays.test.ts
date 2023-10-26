adb22b5e65b4992b092511f9b77644d9c4355806
Jason Nordheim <jason.nordheim@gmail.com>
2023-10-26 11:32:23 -0600

    added tests

--- src/leetCode/medianOfTwoArrays.test.ts
+++ src/leetCode/medianOfTwoArrays.test.ts
@@ -4,10 +4,13 @@ import { findMedianSortedArrays } from "./medianOfTwoArrays";
 const tests: [number[], number[], number][] = [
   [[1, 3], [2], 2],
   [[1, 2], [3, 4], 2.5],
+  [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], 5.5],
+  [[1, 3, 8, 9], [2, 4, 7], 4],
+  [[1, 2, 3, 4, 5], [], 3],
 ];
 
 describe("median of two arrays", () => {
-  test.each(tests)("findMedianSortedArrays(%o, %0) returns %d", (nums1, nums2, result) => {
+  test.each(tests)("findMedianSortedArrays(%o, %o) returns %d", (nums1, nums2, result) => {
     expect(findMedianSortedArrays(nums1, nums2)).toBe(result);
   });
 });
